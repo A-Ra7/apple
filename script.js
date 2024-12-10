80f95c4ce3ae854334942e1a2c973aa159b9b6cc
Артур Ра <77981720+A-Ra7@users.noreply.github.com>
2024-12-10 17:46:49 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,62 @@
+// Предметы
 const items = [
-    { name: "Яблоко 1", x: 90, y: 300 },
-    { name: "Яблоко 2", x: 140, y: 310 },
-    { name: "Яблоко 3", x: 250, y: 310 }
-];
-
-const itemX = [
-    { name: "Червяк", x: 200, y: 300 }
+    { id: 1, name: "Яблоко", x: 90, y: 300 },
+    { id: 2, name: "Яблоко", x: 140, y: 310 },
+    { id: 3, name: "Яблоко", x: 200, y: 300 },
+    { id: 4, name: "Яблоко", x: 250, y: 310 }
 ];
 
 const gameContainer = document.getElementById('game-container');
 const itemList = document.getElementById('itemList');
 
-// Create item list
+const min = 1;
+const max = items.length;
+const itemX = Math.floor(Math.random() * (max - min + 1)) + min;
+let itemNumber = 1;
+
+// Создаём предмет
 items.forEach(item => {
-    // Create target area
     const target = document.createElement('div');
-    target.className = 'target';
+
+    if (item.id == itemX) {
+        target.className = 'target-x';
+    }
+    else {
+        target.className = 'target';
+
+        // Создаём список предметов
+        const listItem = document.createElement('div');
+        listItem.textContent = item.name + " " + itemNumber;
+        itemNumber ++;
+        listItem.id = 'item-' + item.id;
+        itemList.appendChild(listItem);
+    }
+    
     target.style.left = item.x + 'px';
     target.style.top = item.y + 'px';
     gameContainer.appendChild(target);
     
-    // Create list item
-    const listItem = document.createElement('div');
-    listItem.textContent = item.name;
-    listItem.id = 'item-' + item.name.replace(/\s+/g, '-').toLowerCase();
-    itemList.appendChild(listItem);
-    
+    // Проверяем предметы
     target.addEventListener('click', () => {
         target.style.display = 'none';
-        document.getElementById('item-' + item.name.replace(/\s+/g, '-').toLowerCase()).classList.add('found');
+        //Нашли предмет Х
+        if (target.className == 'target-x') {
+            document.querySelectorAll('.target').forEach(target => {
+                target.style.display = 'none';
+             });
+            const worm = document.getElementById('worm');
+            worm.style.display = 'block';
+            const gameButton = document.getElementById('game-button');
+            gameButton.style.display = 'block';
+            setTimeout("alert('Вы нашли яблоко с червяком :O Попробуйте ещё раз!')", 1000); 
+            return
+        }
         
-        // Check if all items found
+        document.getElementById('item-' + item.id).classList.add('found');
+
+        // Нашли все предметы без предмета Х
         const foundItems = document.querySelectorAll('.found').length;
-        if (foundItems === items.length) {
+        if (foundItems === (items.length - 1)) {
             document.querySelectorAll('.target-x').forEach(targetX => {
                 targetX.style.display = 'none';
                 const victory = document.getElementById('victory');
@@ -41,34 +65,11 @@ items.forEach(item => {
                 gameButton.style.display = 'block';
             });
             setTimeout("alert('Поздравляем! Вы нашли все яблочки =)')", 1000); 
-
         }
     });
 });
 
-    // Create item listX
-    itemX.forEach(itemX => {
-    // Create targetX area
-    const targetX = document.createElement('div');
-    targetX.className = 'target-x';
-    targetX.style.left = itemX.x + 'px';
-    targetX.style.top = itemX.y + 'px';
-    gameContainer.appendChild(targetX);
-    
-    // ItemX found
-    targetX.addEventListener('click', () => {
-        document.querySelectorAll('.target').forEach(target => {
-            target.style.display = 'none';
-        });
-        targetX.style.display = 'none';
-        const worm = document.getElementById('worm');
-        worm.style.display = 'block';
-        const gameButton = document.getElementById('game-button');
-        gameButton.style.display = 'block';
-        setTimeout("alert('Вы нашли яблоко с червяком :O Попробуйте ещё раз!')", 1000); 
-    });
-});
-
+// Перезагружаем игру
 function restartGame () {
     window.location.reload();
 }
