74aac00c78fef7d0e560f8acf9167dddd416c8e8
Артур Ра <77981720+A-Ra7@users.noreply.github.com>
2024-12-16 19:59:42 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-// Прелоадер
-window.onload = function () {
-    let preloader = document.getElementById('preloader-container');
-    if (!preloader.classList.contains('loaded')) {
-        preloader.classList.add('loaded');
-    }
-}
-
 // Предметы
 const items = [
     { id: 1, x: 70, y: 300 },
     { id: 2, x: 130, y: 310 },
-    { id: 3, x: 190, y: 300 },
-    { id: 4, x: 250, y: 310 }
+    { id: 3, x: 190, y: 310 },
+    { id: 4, x: 250, y: 310 },
+    { id: 5, x: 100, y: 260 },
+    { id: 6, x: 160, y: 260 },
+    { id: 7, x: 220, y: 270 },
+    { id: 8, x: 160, y: 205 }
 ];
 
 // Название предмета
-const itemName = "Яблоко";
+const itemName = "Яблоко -";
 
 const gameContainer = document.getElementById('game-container');
 const itemList = document.getElementById('itemList');
@@ -37,7 +33,7 @@ items.forEach(item => {
 
         // Создаём список предметов
         const listItem = document.createElement('div');
-        listItem.textContent = itemName + " " + itemNumber;
+        listItem.textContent = itemName + " " + itemNumber + " ";
         itemNumber ++;
         listItem.id = 'item-' + item.id;
         itemList.appendChild(listItem);
@@ -50,11 +46,14 @@ items.forEach(item => {
     // Проверяем предметы
     target.addEventListener('click', () => {
         target.style.display = 'none';
+
         //Нашли предмет Х
-        if (target.className == 'target-x') {
+        if (target.className.includes('target-x')) {
             document.querySelectorAll('.target').forEach(target => {
                 target.style.display = 'none';
              });
+            let gameBg = document.getElementById('game-bg');
+            gameBg.style.opacity = '0.7';
             const worm = document.getElementById('worm');
             worm.style.display = 'block';
             const gameButton = document.getElementById('game-button');
@@ -71,6 +70,8 @@ items.forEach(item => {
         if (foundItems === (items.length - 1)) {
             document.querySelectorAll('.target-x').forEach(targetX => {
                 targetX.style.display = 'none';
+                let gameBg = document.getElementById('game-bg');
+                gameBg.style.opacity = '0.7';
                 const victory = document.getElementById('victory');
                 victory.style.display = 'block';
                 const gameButton = document.getElementById('game-button');
@@ -80,6 +81,31 @@ items.forEach(item => {
         }
     });
 });
+
+// Прелоадер
+window.onload = function () {
+    let preloader = document.getElementById('preloader-container');
+    if (!preloader.classList.contains('loaded')) {
+        preloader.classList.add('loaded');
+    }
+
+    let gameBg = document.getElementById('game-bg');
+    if (!gameBg.classList.contains('enter')) {
+        gameBg.classList.add('enter');
+    }
+
+    document.querySelectorAll('.target').forEach(target => {
+        if (!target.classList.contains('fall')) {
+            target.classList.add('fall');
+        }
+     });
+
+    document.querySelectorAll('.target-x').forEach(targetX => {
+        if (!targetX.classList.contains('fall')) {
+            targetX.classList.add('fall');
+        }
+    });
+}
 
 function restartGame () {
     window.location.reload();
